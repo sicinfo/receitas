b8a86dc4bcf66a1798cb33e3dd4d07285a456f39
moreira <moreiraxx+github_receitas@gmail.com>
2021-09-14 20:10:32 -0300

    corrige

--- public/components/receita.js
+++ public/components/receita.js
@@ -20,26 +20,19 @@ System.register([
     const { Container, Typography, InputBase, Input, Box, List, ListItem } = MaterialUi;
     const { titulo, rendimento, ingredientes = [], preparo } = receita;
 
-    const [values, setState] = useState({})
+    const [values, setState] = useState({ 
+      __quantidade: rendimento.quantidadeMinimo 
+    })
 
     const ListIngredientes = props => {
       const { quantidade, salto } = values[props.descricao];
       return h(ListItem, null, `${quantidade * salto} ${props.descricao}`)
     }
 
-    return h(Container, 
-      {
-        onChange(evt) {
-          const salto = evt.target.value / rendimento.quantidadeMinimo;
-          Object.keys(values).some(descricao => {
-            values[descricao].salto = salto;
-            setState({ ...values });
-          })
-        }
-      }, [
+    return h(Container, null, [
       h(Box, { component: 'h2' }, titulo),
       h(Typography, null, [
-        h(Box, { component: 'span' }, 'rendimento '),
+        h(Box, { component: 'span' }, 'porções '),
         h(Input, {
           type: 'range',
           name: 'InputQuantidade',
@@ -48,8 +41,21 @@ System.register([
             min: rendimento.quantidadeMinimo,
             max: rendimento.quantidadeMaximo,
             step: rendimento.quantidadeSalto
-          }
-        }),
+          },
+          onChange: (evt) => {
+            evt.stopPropagation();
+            const salto = evt.target.value / rendimento.quantidadeMinimo;
+            values.__quantidade = evt.target.value;
+            Object.keys(values)
+              .filter(key => key[0] !== '__')
+              .forEach(key => { values[key].salto = salto });
+            setState({ ...values });
+          }  
+        })
+      ]),
+      h(Typography, null, [
+        h(Box, { component: 'span' }, 'rendimento '),
+        h(Box, { component: 'span' }, values.__quantidade),
         h(Box, { component: 'span' }, ` ${rendimento.unidade} ${rendimento.descricao}`)
       ]),
       h(Box, { component: 'h3' }, `ingredientes`),
@@ -68,149 +74,6 @@ System.register([
       h(List, null, preparo?.modo.map(item => h(ListItem, null, item)))
     ])
   })
-
-
-
-  //   const elems = [];
-    
-  //   elems.push(
-  //       h(Box, { component: 'h2' }, titulo),
-  //       h(Typography, null, [
-  //         h(
-  //           Box,
-  //           {
-  //             component: 'span'
-  //           },
-  //           'rendimento '
-  //         ),
-  //         h(
-  //           Input,
-  //           {
-  //             type: 'range',
-  //             name: 'InputQuantidade',
-  //             onChange: evt => {
-  //               const step = evt.target.value / rendimento.quantidadeMinimo;
-  //             },
-  //             defaultValue: rendimento.quantidadeMinimo,
-  //             inputProps: {
-  //               min: rendimento.quantidadeMinimo,
-  //               max: rendimento.quantidadeMaximo,
-  //               step: rendimento.quantidadeSalto
-  //             }
-  //           }
-  //         ),
-  //         h(
-  //           Box,
-  //           {
-  //             component: 'span'
-  //           },
-  //           ` ${rendimento.unidade} ${rendimento.descricao}`
-  //         )
-  //       ])
-  //     )
-
-  //     if (preparo?.tempo) elems.push(
-  //       h(Typography, null, `tempo de preparo  ${preparo?.tempo}`)
-  //     )
-
-  //     if (ingredientes) elems.push(
-  //       h(Box, { component: 'h3' }, `ingredientes`),
-  //       h(List, null, ingrs.map(ingr => h(ListIngredientes, ingr)))
-  //     )
-
-
-
-
-
-
-
-
-
-
-
-  //   const ingrs = ingredientes.map((ingr, index) => {
-
-  //     const descricao = ` ${ingr.unidade} ${ingr.descricao}`;
-  //     const quantidade = ingr.quantidadeMinimo;
-
-  //     console.log(values);
-        
-  //     setValue(...values, ...Object.fromEntries([[descricao, { quantidade, index }]]));
-
-  //     return { descricao, quantidade }
-  //   });
-
-  
-  //   const Elems = ({ values, setValue }) => {
-
-  //     const elems = [];
-
-  //     if (attributes.key && attributes.key !== 'home' && titulo) {
-  
-  //       elems.push(
-  //         h(Box, { component: 'h2' }, titulo),
-  //         h(Typography, null, [
-  //           h(
-  //             Box,
-  //             {
-  //               component: 'span'
-  //             },
-  //             'rendimento '
-  //           ),
-  //           h(
-  //             Input,
-  //             {
-  //               type: 'range',
-  //               name: 'InputQuantidade',
-  //               onChange: evt => {
-  //                 const step = evt.target.value / rendimento.quantidadeMinimo;
-  //               },
-  //               defaultValue: rendimento.quantidadeMinimo,
-  //               inputProps: {
-  //                 min: rendimento.quantidadeMinimo,
-  //                 max: rendimento.quantidadeMaximo,
-  //                 step: rendimento.quantidadeSalto
-  //               }
-  //             }
-  //           ),
-  //           h(
-  //             Box,
-  //             {
-  //               component: 'span'
-  //             },
-  //             ` ${rendimento.unidade} ${rendimento.descricao}`
-  //           )
-  //         ])
-  //       )
-
-  //       if (preparo?.tempo) elems.push(
-  //         h(Typography, null, `tempo de preparo  ${preparo?.tempo}`)
-  //       )
-
-  //       if (ingredientes) elems.push(
-  //         h(Box, { component: 'h3' }, `ingredientes`),
-  //         h(List, null, ingrs.map(ingr => h(ListIngredientes, ingr)))
-  //       )
-
-  //           // return h(ListItem, null, `${ingr.quantidadeMinimo} ${ingr.unidade} ${ingr.descricao}`)
-
-  //       if (preparo?.modo) elems.push(
-  //         h(Box, { component: 'h3' }, `modo de preparo`),
-  //         h(List, null, preparo?.modo.map(item => h(ListItem, null, item)))
-  //       )
-
-  //     }
-
-  //     return h(Container, { component: 'section' }, elems);
-  //   }
-
-  //   return h(() => {
-  //     const [values, setValue] = useState({});
-
-  //     return h(Elems, { values, setValue });
-  //   })
-
-  // });
 
   return {
     setters: [
