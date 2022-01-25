642cf19e46dc2294cba32789e3b3da05931214ce
sicinfo <moreiraxx+github@gmail.com>
2022-01-24 23:02:52 -0300

    index server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+/**
+ * application receitas
+ * 
+ * powered by moreira 2022-01-24
+ */
+'use strict';
+
+const { Router, loggin } = require('sicinfo-router')(__filename);
+
+module.exports = class extends Router {
+  
+}
