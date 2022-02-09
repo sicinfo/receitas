d6f878a10677a1de2c63cac4b8dc3addb26e4465
moreira <moreiraxx+github-receitas@gmail.com>
2022-02-09 20:43:46 -0300

    atualizar

--- public/components/receita.js
+++ public/components/receita.js
@@ -5,119 +5,112 @@
  */
 'use strict'
 
-System.register(['axios', 'react'], async (_export, _context) => {
-  
+System.register(['axios', 'react'], (_export, _context) => {
+
   /** @type {ReactJs} */ let ReactJs;
   /** @type {AxiosInstance} */ let axios;
-})
-  
-  const getReceita = async (/** @type{string} */ name) => {
-    return await axios.get(`receita/${name}`).then(({ data }) => data);
-  }
 
   /**
    * @param {Record<string,string>} arg
    */
-  const _default = ({name}) => {
+  const _default = ({ name }) => {
     const { createElement: h, useState, Component } = ReactJs;
 
-    const data = getReceita(name);
+    /** @type {[any, any]} */
+    const [_d, setData] = useState(null);
     
-    const { 
-      titulo: tit,
-      rendimento: { 
-        descricao: desc, 
-        unidade: und, 
-        quantidade: { 
-          minimo: min, 
-          maximo: max 
-        }, 
-        preparo: prep 
-      },
-      ingredientes: ings
-    } = data;
+    /** @type {[any, any]} */
+    const [_i, setItens] = useState({});
 
-  // // } = /** @type {any} */ (await axios.get(`receita/${name}`).then(({ data }) => data));
-    
-    // /** @type {[any,any]} */
-    // const [values, setState] = useState({
-    //   __quantidade: min,
-    //   __custo: 0,
-    //   __salto: 1
-    // });
-
-    // /** 
-    //  * @param {Record<string,string>} props
-    //  */
-    // const ListIngredientes = props => {
-    //   const { descricao: desc } = props;
-    //   const { quantidade: qtd, salto } = values[desc];
-    //   return h('li', null, `${qtd * values.__salto} ${desc}`)
-    // }
-
-    // return h('div', null, tit)
-     
-    // return h('div', null, [
-    //   h('h2', null, tit),
-    //   h('div', null, [
-    //     h('span', null, 'porções '),
-    //     h('input', {
-    //       type: 'range',
-    //       name: 'InputQuantidade',
-    //       defaultValue: min,
-    //       inputProps: { min, max, step: min },
-    //       onChange: (evt) => {
-    //         evt.stopPropagation();
-    //         const { value, step } = /** @type {Record<string,any>} */ (evt.target);
-    //         values.__salto = 0 + value / step;
-    //         values.__quantidade = value;
-    //         setState({ ...values });
-    //       }
-    //     })
-    //   ]),
-    //   h('div', null, [
-    //     h('span', null, 'rendimento '),
-    //     h('span', null, values.__quantidade),
-    //     h('span', null, ` ${und} ${desc}`)
-    //   ]),
-    //   h('h3', null, [
-    //     h('span', null, 'ingredientes  -  custo R$ '),
-    //     h('span', null, Math.ceil(values.__custo * values.__salto * 100) / 100)
-    //   ]),
-    //   h('ul', null, /** @type {Record<string,any>[]} */ (ings).map(({unidade: und, descricao: desc, quantidade: qtd, custo: cust }) => {
-
-    //     // const key = (a => `${custos[a].unidade} ${a}`)(ingr.descricao);
-    //     const key = `${und} ${desc}`;
-
-    //     if (!(key in values)) {
-    //       values[key] = {
-    //         quantidade: qtd,
-    //         custo: cust
-    //       };
-    //       values.__custo += values[key].custo
-    //       setState({ ...values });
-    //     }
-
-    //     return h(ListIngredientes, { descricao: key });
-    //   })),
-    //   h('h3', null, `modo de preparo`),
-    //   h('ol', null, prep?.modo.map((/** @type {string} */ item) => h('li', null, item)))
-    // ])
-// }
-
-  return {
-    setters: [
-      a => { axios = a.default },
-      a => { ReactJs = a.default }
-    ],
-
-    execute: () => {
-      _export('version', '0.1.0');
-
-      _export('default', _default)
+    if (!_d) {
+      axios.get(`receita/${name}`).then(({ data }) => {
+        const {
+          titulo: tit,
+          rendimento: {
+            descricao: desc,
+            unidade: und,
+            quantidade: {
+              minimo: min,
+              maximo: max
+            },
+            preparo: prep
+          },
+          ingredientes: ings
+        } = data;
+        setData({ tit, desc, und, min, max, prep, ings, cust: 0, qtd: 0, salt: 1 })
+      });
+      return h('div', null, 'waiting...');
     }
+    
+    /** 
+     * @param {any} props
+     */
+    const ListIngredientes = props => {
+      const { descricao: desc } = props;
+      const { qtd, salt } = _d[desc];
+      return h('li', null, `${qtd * salt} ${desc}`)
+    }
+
+    return h('div', null, [
+      h('h2', null, _d.tit),
+      h('div', null, [
+        h('span', null, 'porções '),
+        h('input', {
+          type: 'range',
+          name: 'InputQuantidade',
+          defaultValue: _d.min,
+          inputProps: { 
+            min: _d.min, 
+            max: _d.max, 
+            step: _d.min 
+          },
+          onChange: (evt) => {
+            evt.stopPropagation();
+            const { value, step } = /** @type {Record<string,number>} */ (/** @type {unknown} */ (evt.target));
+            _d.qtd = 1 * value;
+            _d.salt = _d.qtd / (1 * step);
+            setData({ ..._d });
+          }
+        })
+      ]),
+      h('div', null, [
+        h('span', null, 'rendimento '),
+        h('span', null, _d.qtd),
+        h('span', null, ` ${_d.und} ${_d.desc}`)
+      ]),
+      h('h3', null, [
+        h('span', null, 'ingredientes  -  custo R$ '),
+        h('span', null, Math.ceil(_d.cust * _d.salt * 100) / 100)
+      ]),
+      h('ul', null, /** @type {Record<string,any>[]} */ (_d.ings).map(({unidade: und, descricao: desc, quantidade: qtd, custo: cust }) => {
+
+        // const key = (a => `${custos[a].unidade} ${a}`)(ingr.descricao);
+        const key = `${und} ${desc}`;
+
+        if (!(key in _i)) {
+          _i[key] = {qtd, cust};
+          _d.cust += _i[key].cust
+          setValues({ ..._v });
+        }
+
+        return h(ListIngredientes, { descricao: key });
+      })),
+      h('h3', null, `modo de preparo`),
+      h('ol', null, _d.prep?.modo.map((/** @type {string} */ item) => h('li', null, item)))
+    ])
   }
-})
+
+    return {
+      setters: [
+        a => { axios = a.default },
+        a => { ReactJs = a.default }
+      ],
+      execute: () => {
+        _export('version', '0.1.0');
+        _export('default', _default)
+      }
+    }
+  })
 
 
 // /**
@@ -209,3 +202,16 @@ System.register(['axios', 'react'], async (_export, _context) => {
 //   }
 
 // })
+
+
+
+
+
+    // // } = /** @type {any} */ (await axios.get(`receita/${name}`).then(({ data }) => data));
+
+    // /** @type {[any,any]} */
+    // const [values, setState] = useState({
+    //   __quantidade: min,
+    //   __custo: 0,
+    //   __salto: 1
+    // });
